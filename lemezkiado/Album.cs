b614d76f3ba179d1d251b437cfffcff45f8d7f9a
kizmusnoel <kizmus.noel@students.jedlik.eu>
2025-11-20 12:29:21 +0100

    init

---
+++ lemezkiado/Album.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Lemezkiado
+{
+    public class Album
+    {
+        public int id { get; set; }
+        public string albumName { get; set; } = "";
+        public string artistName { get; set; } = "";
+        public DateTime releaseDate { get; set; }
+        public float price { get; set; }
+        public int streams { get; set; }
+        public int copiesSold { get; set; }
+        public string genre { get; set; }
+        public bool explicitAlbum { get; set; }
+        public string[] trackList { get; set; } = [];
+
+        //public void List<Album> LoadFromJson(string filename)
+        //{
+        //    var jsonContent = System.IO.File.ReadAllText(filename, Encoding.UTF8);
+        //    /*
+        //    var albums = JsonSerializer.Deserialize<List<Album>>(jsonContent, new JsonSerializerOptions()
+        //    {
+        //        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        //    });
+        //    */
+        //    return albums ?? new List<Album>();
+
+        //}
+    }
+}
